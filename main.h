dc76dfe509e36144a7f0db2bf5e7b11a203c4eda
AlexNduta <wasremoved@gmail.com>
2023-04-23 13:29:56 +0300

    edits to main,h

---
+++ main.h
@@ -0,0 +1,11 @@
+#ifndef MAIN_H
+#define MAIN_H
+#include <stdarg.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+int _printf(const char *format, ...);
+int check_buff(char *our_buffer, int length);
+char *create_buff(void);
+void write_to_buffer(char *our_buffer, int length, va_list mylist);
+#endif
