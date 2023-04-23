772cf3dda09ffe12410e5df4f8be5103ff247573
Joseph Miseda <misedajoseph@gmail.com>
2023-04-23 12:33:29 +0300

    c handler

---
+++ print-c.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *print-c - handle the c specifier in the format string
+ *@mylist: the second arguments of our custom printf
+ *
+ *Return: character pointer or string array
+ */
+char *print-c(va_list mylist)
+{
+	char *s = malloc(sizeof(char) * 2);
+
+	if (s == NULL)
+		return (NULL);
+
+	s[0] = va_arg(mylist, int);
+	s[1] = '\0';
+
+	return (s);
+}
