669f9b35fcca4b797051e2e7fbe0905c20070b35
AlexNduta <wasremoved@gmail.com>
2023-04-24 19:09:29 +0300

    fix errors on _printf: I had a betty issue but Its sorted

--- main.h
+++ main.h
@@ -9,6 +9,8 @@ char *spec_s(va_list mylist);
 char *spec_d(va_list mylist);
 int _strlen(char *s);
 int _printf(const char *format, ...);
+
+/* Helper functions */
 int check_buff(char *our_buffer, int length);
 char *create_buff(void);
 void write_to_buffer(char *our_buffer, int length, va_list mylist);
